8b5febf19a66d728a4c21c77a2a518c190ac7bf7
Bruno Bucci <bruno@brunobucci.com>
2025-02-09 12:59:15 -0300

    Ajustes

--- init-mongo.js
+++ init-mongo.js
@@ -3,4 +3,10 @@ db = db.getSiblingDB('Hackathon'); // Cria ou acessa o banco
 // Criação da coleção "usuario" sem inserir documentos
 db.createCollection("usuario");
 
-print("Banco de dados e coleções configurados com sucesso!");
+db.createUser({
+    user: "soat7grupo5",
+    pwd: "Hc9YCOqLHPsKvf7e",
+    roles: [{ role: "root", db: "Hackathon" }]
+});
+
+print("Banco de dados e coleções configurados com sucesso!"); 
